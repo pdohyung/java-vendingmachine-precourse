40df585e9df74ad6de0d76b92c30b5fdc012bf00
kimdohyung <pdohyung@gmail.com>
2023-12-12 23:51:17 +0900

    feat(OutputView): 상품 가격 및 금액 입력 예외 처리

--- src/main/java/vendingmachine/view/InputValidator.java
+++ src/main/java/vendingmachine/view/InputValidator.java
@@ -4,12 +4,16 @@ import vendingmachine.domain.Product;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputValidator {
-    private static final String INVALID_NUMBER_FORMAT_MESSAGE = "숫자 형식 입력이 아닙니다.";
+    private static final String INVALID_NUMBER_PATTERN = "^[1-9]\\d*$";
+    private static final String INVALID_NUMBER_FORMAT_MESSAGE = "올바른 숫자 형식 입력이 아닙니다.";
+    private static final String INVALID_PRICE_FORMAT_MESSAGE = "상품 가격은 100원부터 시작하며, 10원으로 나누어떨어져야 한다.";
 
     public static int validateVendingMachineMoney(String input) {
+        validateNumberPattern(input);
         return convertStringToInteger(input);
     }
 
@@ -32,6 +36,7 @@ public class InputValidator {
     public static Product convertStringToProduct(String productString) {
         String[] parts = productString.replace("[", "").replace("]", "").split(",");
         String name = parts[0];
+        validateProductPrice(Integer.parseInt(parts[1]));
         int price = Integer.parseInt(parts[1]);
         int quantity = Integer.parseInt(parts[2]);
         return new Product(name, price, quantity);
@@ -47,6 +52,18 @@ public class InputValidator {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_NUMBER_FORMAT_MESSAGE);
+        }
+    }
+
+    private static void validateNumberPattern(String input) {
+        if (!Pattern.matches(INVALID_NUMBER_PATTERN, input)) {
+            throw new IllegalArgumentException(INVALID_NUMBER_FORMAT_MESSAGE);
+        }
+    }
+
+    private static void validateProductPrice(int price) {
+        if (price < 100 || (price % 10) != 0) {
+            throw new IllegalArgumentException(INVALID_PRICE_FORMAT_MESSAGE);
         }
     }
 }
--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -2,10 +2,12 @@ package vendingmachine.view;
 
 import vendingmachine.domain.Coin;
 
-import java.text.MessageFormat;
 import java.util.EnumMap;
 
+import static java.text.MessageFormat.*;
+
 public class OutputView {
+    private static final String ERROR_MESSAGE_FORMAT = "[ERROR] {0}";
     private final String PRINT_COIN_FORMAT = "{0}원 - {1}개";
     private final String PRINT_AMOUNT_FORMAT = "\n투입 금액: {0}원";
     private final String PRINT_COINS_HAVE_VENDING_MACHINE = "\n자판기가 보유한 동전";
@@ -14,17 +16,21 @@ public class OutputView {
     public void printCoinsHaveVendingMachine(EnumMap<Coin, Integer> coins) {
         System.out.println(PRINT_COINS_HAVE_VENDING_MACHINE);
         coins.keySet().forEach(coin ->
-                System.out.println(MessageFormat.format(PRINT_COIN_FORMAT, coin.getAmount(), coins.get(coin))));
+                System.out.println(format(PRINT_COIN_FORMAT, coin.getAmount(), coins.get(coin))));
     }
 
     public void printAmount(int amount) {
-        System.out.println(MessageFormat.format(PRINT_AMOUNT_FORMAT, amount));
+        System.out.println(format(PRINT_AMOUNT_FORMAT, amount));
     }
 
     public void printChanges(EnumMap<Coin, Integer> coins) {
         System.out.println(PRINT_CHANGES);
         coins.keySet().stream()
                 .filter(coin -> coins.get(coin) != 0)
-                .forEach(coin -> System.out.println(MessageFormat.format(PRINT_COIN_FORMAT, coin.getAmount(), coins.get(coin))));
+                .forEach(coin -> System.out.println(format(PRINT_COIN_FORMAT, coin.getAmount(), coins.get(coin))));
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(format(ERROR_MESSAGE_FORMAT, errorMessage));
     }
 }
