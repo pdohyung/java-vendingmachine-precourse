f5a22cb1fef42886b265436ff9dee7ead9846324
kimdohyung <pdohyung@gmail.com>
2023-12-12 20:15:58 +0900

    feat(InputView): 상품명과 가격, 수량을 입력한다.

--- src/main/java/vendingmachine/view/InputValidator.java
+++ src/main/java/vendingmachine/view/InputValidator.java
@@ -1,10 +1,37 @@
 package vendingmachine.view;
 
+import vendingmachine.domain.Product;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputValidator {
     private static final String INVALID_NUMBER_FORMAT_MESSAGE = "숫자 형식 입력이 아닙니다.";
 
     public static int validateVendingMachineMoney(String input) {
         return convertStringToInteger(input);
+    }
+
+    public static List<Product> validateProducts(String input) {
+        List<String> products = splitProductsInput(input);
+
+        return products.stream()
+                .map(InputValidator::convertStringToProduct)
+                .collect(Collectors.toList());
+    }
+
+    public static Product convertStringToProduct(String productString) {
+        String[] parts = productString.replace("[", "").replace("]", "").split(",");
+        String name = parts[0];
+        int price = Integer.parseInt(parts[1]);
+        int quantity = Integer.parseInt(parts[2]);
+        return new Product(name, price, quantity);
+    }
+
+    private static List<String> splitProductsInput(String input) {
+        return Arrays.stream(input.split(";"))
+                .collect(Collectors.toList());
     }
 
     private static int convertStringToInteger(String input) {
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,10 +1,20 @@
 package vendingmachine.view;
 
-import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.domain.Product;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class InputView {
+    Scanner sc = new Scanner(System.in);
+
     public int inputVendingMachineMoney() {
         System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
-        return InputValidator.validateVendingMachineMoney(Console.readLine());
+        return InputValidator.validateVendingMachineMoney(sc.nextLine());
+    }
+
+    public List<Product> inputProducts() {
+        System.out.println("\n상품명과 가격, 수량을 입력해 주세요.");
+        return InputValidator.validateProducts(sc.nextLine());
     }
 }
