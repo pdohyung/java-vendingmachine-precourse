57cf7b0919bc42ad1577342b2717aaa9a14ebc91
kimdohyung <pdohyung@gmail.com>
2023-12-12 20:46:03 +0900

    feat(OutputView): 투입 금액을 출력한다.

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -7,9 +7,16 @@ import java.util.EnumMap;
 
 public class OutputView {
     private final String PRINT_COIN_FORMAT = "{0}원 - {1}개";
+    private final String PRINT_AMOUNT_FORMAT = "\n투입 금액: {0}원";
+    private final String PRINT_COINS_HAVE_VENDING_MACHINE = "\n자판기가 보유한 동전";
 
     public void printCoinsHaveVendingMachine(EnumMap<Coin, Integer> coins) {
+        System.out.println(PRINT_COINS_HAVE_VENDING_MACHINE);
         coins.keySet().forEach(coin ->
                 System.out.println(MessageFormat.format(PRINT_COIN_FORMAT, coin.getAmount(), coins.get(coin))));
+    }
+
+    public void printAmount(int amount) {
+        System.out.println(MessageFormat.format(PRINT_AMOUNT_FORMAT, amount));
     }
 }
